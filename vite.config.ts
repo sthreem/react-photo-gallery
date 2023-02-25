8a35941f1d1160b9126543254ce8ffc0ab2d7e00
Sthreem <milivojevic.ste@gmail.com>
2023-02-25 15:36:26 -0500

    Additional eslint prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import typescript from '@rollup/plugin-typescript';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), typescript()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
